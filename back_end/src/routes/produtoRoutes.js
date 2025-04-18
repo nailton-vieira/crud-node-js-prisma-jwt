300157bf98f1378f1b2d25d58dc8febc0f17a725
nailton-vieira <nailtonvieira@gmail.com>
2025-04-17 22:52:44 -0300

    Enviando atulizações de rotas

---
+++ back_end/src/routes/produtoRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const produtoController = require('../controllers/produtoController');
+
+router.get('/', produtoController.getAllProdutos);
+router.get('/:id', produtoController.getProdutoById);
+router.post('/', authMiddleware(), produtoController.createProduto);
+router.put('/:id', authMiddleware(), produtoController.updateProduto);
+router.delete('/:id', authMiddleware(), produtoController.deleteProduto);
+
+module.exports = router;
