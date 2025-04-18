0fa9c7afa4fa6ff01ef2634eff83eadb196d31b6
nailton-vieira <nailtonvieira@gmail.com>
2025-04-17 22:19:28 -0300

    Enviando arquivos client prisma e jwt ao repo

---
+++ back_end/src/utils/jwtUtils.js
@@ -0,0 +1,17 @@
+
+
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const generateToken = (userId, funcao) => {
+  return jwt.sign({ userId, funcao }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { generateToken, verifyToken };
